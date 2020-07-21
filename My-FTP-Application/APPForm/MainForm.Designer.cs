6b1db635b997aad1b92389d6f2ff9223846aa703
wycwcq <1193032407@qq.com>
2020-07-21 17:09:05 +0800

    增加图形界面

---
+++ My-FTP-Application/APPForm/MainForm.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace APPForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("计算机");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tstbHost = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnLogin = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.treeLocal = new System.Windows.Forms.TreeView();
+            this.menuLocal = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.menuLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripLabel4 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox3 = new System.Windows.Forms.ToolStripTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.menuRightKey = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.menuLocal.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.tstbHost,
+            this.toolStripLabel4,
+            this.toolStripTextBox3,
+            this.toolStripSeparator1,
+            this.toolStripLabel2,
+            this.toolStripTextBox1,
+            this.toolStripSeparator2,
+            this.toolStripLabel3,
+            this.toolStripTextBox2,
+            this.toolStripSeparator3,
+            this.btnLogin});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(951, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(44, 22);
+            this.toolStripLabel1.Text = "主机：";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+            // 
+            // tstbHost
+            // 
+            this.tstbHost.BackColor = System.Drawing.SystemColors.Window;
+            this.tstbHost.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.tstbHost.Name = "tstbHost";
+            this.tstbHost.Size = new System.Drawing.Size(100, 25);
+            this.tstbHost.Text = "192.168.1.45";
+            this.tstbHost.ToolTipText = "输入主机地址";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(56, 22);
+            this.toolStripLabel2.Text = "用户名：";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            this.toolStripTextBox1.Text = "admin";
+            this.toolStripTextBox1.ToolTipText = "输入用户名";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(44, 22);
+            this.toolStripLabel3.Text = "密码：";
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(100, 25);
+            this.toolStripTextBox2.Text = "123456";
+            this.toolStripTextBox2.ToolTipText = "请输入密码";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Image = global::APPForm.Properties.Resources.ftp;
+            this.btnLogin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(76, 22);
+            this.btnLogin.Text = "点击连接";
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(329, 551);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.toolStrip2);
+            this.tabPage1.Controls.Add(this.treeLocal);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(321, 525);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "本地驱动器";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage2);
+            this.tabControl2.Location = new System.Drawing.Point(331, 29);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(620, 551);
+            this.tabControl2.TabIndex = 2;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(612, 525);
+            this.tabPage2.TabIndex = 0;
+            this.tabPage2.Text = "FTP服务器";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // treeLocal
+            // 
+            this.treeLocal.ContextMenuStrip = this.menuLocal;
+            this.treeLocal.ImageIndex = 0;
+            this.treeLocal.ImageList = this.imgList;
+            this.treeLocal.Location = new System.Drawing.Point(4, 32);
+            this.treeLocal.Name = "treeLocal";
+            treeNode1.Name = "节点0";
+            treeNode1.Text = "计算机";
+            this.treeLocal.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeLocal.SelectedImageIndex = 0;
+            this.treeLocal.Size = new System.Drawing.Size(314, 476);
+            this.treeLocal.TabIndex = 0;
+            this.treeLocal.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeLocal_BeforeExpand);
+            this.treeLocal.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeLocal_AfterSelect);
+            // 
+            // menuLocal
+            // 
+            this.menuLocal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuLoad});
+            this.menuLocal.Name = "menuLocal";
+            this.menuLocal.Size = new System.Drawing.Size(101, 26);
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "computer.gif");
+            this.imgList.Images.SetKeyName(1, "drive.gif");
+            this.imgList.Images.SetKeyName(2, "folder.ico");
+            this.imgList.Images.SetKeyName(3, "file.ico");
+            // 
+            // menuLoad
+            // 
+            this.menuLoad.Name = "menuLoad";
+            this.menuLoad.Size = new System.Drawing.Size(100, 22);
+            this.menuLoad.Text = "上传";
+            this.menuLoad.Click += new System.EventHandler(this.menuLoad_Click);
+            // 
+            // toolStripLabel4
+            // 
+            this.toolStripLabel4.Name = "toolStripLabel4";
+            this.toolStripLabel4.Size = new System.Drawing.Size(44, 22);
+            this.toolStripLabel4.Text = "端口：";
+            // 
+            // toolStripTextBox3
+            // 
+            this.toolStripTextBox3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.toolStripTextBox3.Name = "toolStripTextBox3";
+            this.toolStripTextBox3.Size = new System.Drawing.Size(100, 25);
+            this.toolStripTextBox3.Text = "21";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 583);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(951, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(76, 17);
+            this.toolStripStatusLabel1.Text = "欢迎使用……";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2});
+            this.toolStrip2.Location = new System.Drawing.Point(3, 3);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(315, 25);
+            this.toolStrip2.TabIndex = 1;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = global::APPForm.Properties.Resources.ww;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(100, 22);
+            this.toolStripButton2.Text = "返回上级目录";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // menuRightKey
+            // 
+            this.menuRightKey.Name = "menuRightKey";
+            this.menuRightKey.Size = new System.Drawing.Size(61, 4);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(951, 605);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabControl2.ResumeLayout(false);
+            this.menuLocal.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox tstbHost;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripButton btnLogin;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TreeView treeLocal;
+        private System.Windows.Forms.ContextMenuStrip menuLocal;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel4;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox3;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripMenuItem menuLoad;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ContextMenuStrip menuRightKey;
+    }
+}
+
---
+++ My-FTP-Application/APPForm/MainForm.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APPForm
+{
+    public partial class MainForm : Form
+    {
+        //private FtpHelper ftpHelper;
+
+        private  DateTime clickTime;
+
+        private bool isClicked = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //初始化
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            InitTreeView();
+        }
+
+        private void InitTreeView()
+        {
+            string[] drives = Environment.GetLogicalDrives();
+            int i = 0;
+            foreach (string drive in drives)
+            {
+                DriveInfo d = new DriveInfo(drive);
+                if ((d.DriveType & DriveType.Fixed) == DriveType.Fixed)
+                {
+                    string drive1 = drive.Substring(0, drive.Length - 1);
+                    this.treeLocal.Nodes[0].Nodes.Add(drive1);
+                    this.treeLocal.Nodes[0].Nodes[i].ImageIndex = 1;
+                    this.treeLocal.Nodes[0].Nodes[i].SelectedImageIndex = 1;
+                    this.treeLocal.Nodes[0].Nodes[i].Tag = drive1;
+                    this.treeLocal.Nodes[0].Nodes[i].Nodes.Add("");//增加一个空白节点，看起来是加号
+                    i++;
+                }
+            }
+        }
+
+        //树菜单相关代码
+        private void treeLocal_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            if (e.Node.Level > 0)
+            {
+                //点击之前，先填充节点：
+                string path = e.Node.FullPath.Substring(e.Node.FullPath.IndexOf("\\") + 1) + "\\";
+                e.Node.Nodes.Clear();
+                //string path = string.Format("{0}{1}",e.Node.Tag,this.treeLocal.PathSeparator);
+                string[] files = Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly);
+                int i = 0;
+                foreach (string file in files)
+                {
+                    FileInfo f = new FileInfo(file);
+                    if ((f.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden && (f.Attributes & FileAttributes.System) != FileAttributes.System)
+                    {
+                        e.Node.Nodes.Add(Path.GetFileName(file));
+                        e.Node.Nodes[i].ImageIndex = 3;
+                        e.Node.Nodes[i].SelectedImageIndex = 3;
+                        e.Node.Nodes[i].Tag = file;
+                        i++;
+                    }
+                }
+                string[] dirs = Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly);
+                foreach (string dir in dirs)
+                {
+                    DirectoryInfo d = new DirectoryInfo(dir);
+                    if ((d.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden && (d.Attributes & FileAttributes.System) != FileAttributes.System)
+                    {
+                        e.Node.Nodes.Add(Path.GetFileName(dir));
+                        e.Node.Nodes[i].ImageIndex = 2;
+                        e.Node.Nodes[i].SelectedImageIndex = 2;
+                        e.Node.Nodes[i].Tag = dir;
+                        e.Node.Nodes[i].Nodes.Add("");//增加一个空白节点，看起来是加号
+                        i++;
+                    }
+                }
+            }
+        }
+        private void treeLocal_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Level > 0 && e.Node.ImageIndex == 3)
+            {
+                this.menuLocal.Enabled = true;
+                string path = e.Node.FullPath.Substring(e.Node.FullPath.IndexOf("\\") + 1);
+                this.menuLoad.Tag = path;
+            }
+            else
+            {
+                this.menuLocal.Enabled = false;
+            }
+        }
+
+        /* 下面是ftp相关代码*/
+        /*-------------------------------------------*/
+        //右键点击上传
+        private void menuLoad_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            //如果就目录,先设置上级目录，再列出目录
+            //ftpHelper.SetPrePath();
+            //this.ListDirectory();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    if (CheckInfo())
+            //    {
+            //        string ipAddr = this.txtAddress.Text.Trim();
+            //        string port = this.txtPort.Text.Trim();
+            //        string userName = this.txtUserName.Text.Trim();
+            //        string password = this.txtPassword.Text.Trim();
+            //        ftpHelper = new FtpHelper(ipAddr, port, userName, password);
+            //        ListDirectory();
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
+        }
+        //private bool CheckInfo()
+        //{
+        //    //string ipAddr = this.txtAddress.Text.Trim();
+        //    //string port = this.txtPort.Text.Trim();
+        //    //string userName = this.txtUserName.Text.Trim();
+        //    //string password = this.txtPassword.Text.Trim();
+        //    //if (string.IsNullOrEmpty(ipAddr) || string.IsNullOrEmpty(port) || string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+        //    //{
+        //    //    MessageBox.Show("请输入登录信息");
+        //    //    return false;
+        //    //}
+        //    //return true;
+        //}
+    }
+}
